f0ed2e4615dc94d2baf129f6dfb8738384efb78d
nicolas <ninigo93@gmail.com>
2024-12-16 00:08:02 -0300

    se creo un pagina para ver las detalles de las peliculas

---
+++ script.js
@@ -0,0 +1,61 @@
+let ApiKey = '5f203800f14737a944702abee8a9b75e';
+
+let URL = 'https://api.themoviedb.org/3/search/movie'
+
+let urlImg = 'https://image.tmdb.org/t/p/w500'
+
+document.getElementById('searchButton').addEventListener('click', buscarPelicula)
+
+function buscarPelicula() {
+    let peliculaIngresada = document.getElementById('searchInput').value
+
+    fetch(`${URL}?api_key=${ApiKey}&query=${peliculaIngresada}`)
+        .then(response => response.json())
+        .then(response => cargarPeliculas(response.results))
+}
+
+
+
+function cargarPeliculas(movies) {
+
+    console.log(movies)
+
+    let contenedorPeliculas = document.getElementById('results');
+
+    contenedorPeliculas.innerHTML = "";
+
+    if (!movies.length === 0) {
+        contenedorPeliculas.innerHTML='<p>No se encontraron resultados para la busqueda.</p>';
+        return
+    }else{
+        movies.forEach(movie => {
+            let movieDiv = document.createElement('div');
+            movieDiv.classList.add('movie')         
+            
+            let titulo = document.createElement('h2');
+            titulo.textContent = movie.title
+
+            let lanzamiento = document.createElement('p');
+            lanzamiento.textContent = `Fecha de lanzamiento: ${movie.release_date}`
+
+            let descripcion = document.createElement('p');
+            descripcion.textContent = `Descripcion: ${movie.overview}`
+
+            let srcPoster = urlImg+movie.poster_path 
+
+            let poster = document.createElement('img')
+            poster.src = srcPoster
+
+            movieDiv.appendChild(poster)
+            movieDiv.appendChild(titulo)
+            movieDiv.appendChild(lanzamiento)
+            movieDiv.appendChild(descripcion)
+
+            contenedorPeliculas.appendChild(movieDiv)
+        });
+    }
+}
+
+
+
+
